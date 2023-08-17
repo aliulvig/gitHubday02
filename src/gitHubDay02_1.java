7a144db7848b0bdcf8b26ba26a05834311cd83d6
Ali Ulvi Girgin <aliulvigirgin@gmail.com>
2023-08-17 16:09:42 +0300

    3.versiyon

--- src/gitHubDay02_1.java
+++ src/gitHubDay02_1.java
@@ -2,6 +2,7 @@ public class gitHubDay02_1 {
 
     public static void main(String[] args) {
         System.out.println("Tekrardan merhaba gitHub");
+        System.out.println("ikinci satir eklendi");
 
     }
 }
